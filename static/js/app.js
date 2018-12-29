4d2cd4bf338e716125eabb909ade0b0388791dec
BuildTools <unconfigured@null.spigotmc.org>
2018-12-29 21:23:55 +0100

    Make the Application Work

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,33 @@
-// from data.js
-var tableData = data;
+const tableData = data;
 
-// YOUR CODE HERE!
+document.getElementById("formDate").addEventListener("submit", (ev) => {
+	ev.preventDefault();
+	
+	const inputDateValue = document.getElementById("datetime").value;
+
+	if (inputDateValue == "" || inputDateValue == null) return;
+
+	let html = "";
+
+	tableData.forEach((el, i) => {
+		
+		let dataTime = el.datetime;
+		
+		if (inputDateValue == el.datetime) {
+			html += `
+				<tr class="text-center">
+					<td>${el.datetime}</td>
+      				<td>${el.city}</td>
+      				<td>${el.state}</td>
+      				<td>${el.country}</td>
+      				<td>${el.shape}</td>
+      				<td>${el.durationMinutes}</td>
+      				<td>${el.comments}</td>
+    			</tr>
+			`;
+		}
+
+		document.getElementById("insertResults").innerHTML = html;
+
+	});
+});
